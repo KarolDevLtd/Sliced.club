8f675ab33e01b66640f5b2cd8d6c8efdb2bd6bda
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2024-03-20 06:13:09 +0000

    Providers arrow function

--- client/src/providers/index.tsx
+++ client/src/providers/index.tsx
@@ -1,18 +1,18 @@
 import React, { ReactNode } from "react";
 import { TRPCReactProvider } from "~/trpc/react";
-import { WalletProvider } from "./WalletProvider";
+import { WalletProvider } from "./walletprovider";
 
 interface Props {
   children?: ReactNode;
   // any props that come into the component
 }
 
-function Providers({ children, ...props }: Props) {
+const Providers = ({ children, ...props }: Props) => {
   return (
     <WalletProvider>
       <TRPCReactProvider>{children}</TRPCReactProvider>;
     </WalletProvider>
   );
-}
+};
 
 export default Providers;
