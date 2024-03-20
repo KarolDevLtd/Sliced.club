33f9bbce4e72959a0277ab50d351287c30d1ae19
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-03-20 22:03:43 +0100

    feat: home page nav

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+  const exampleUserId = "69e8f4d1";
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/login">Login</Link>
+      <Link href="/signup">Register</Link>
+      <Link href={`/profile/${exampleUserId}`}>My Profile</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/group/create">Create Croup</Link>
+    </div>
+  );
 }
