442bc43f6793ae328ac3f7bcc31ad6a788e4579f
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-03-22 21:50:41 +0100

    feat: reorganise links

--- client/src/app/_components/navbar/index.tsx
+++ client/src/app/_components/navbar/index.tsx
@@ -2,10 +2,24 @@
 import React from 'react';
 import { WalletButton } from '~/app/_components/walletbutton';
 
+import Link from 'next/link';
+
 export const Navbar = () => {
+	const exampleUserId = '69e8f4d1';
+
 	return (
 		<header>
-			<p>Sliced</p>
+			<ul>
+				<li>
+					<Link href="/">Sliced</Link>
+				</li>
+				<li>
+					<Link href="/login">Login</Link> | <Link href="/register">Register</Link>
+				</li>
+				<li>
+					<Link href={`/profile/${exampleUserId}`}>My Profile</Link>
+				</li>
+			</ul>
 			<WalletButton />
 		</header>
 	);
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,21 +1,10 @@
 import Link from 'next/link';
 
 export default function Home() {
-	const exampleUserId = '69e8f4d1';
-
 	return (
 		<div>
-			<h1>Home</h1>
+			<h1>Sliced</h1>
 			<ul>
-				<li>
-					<Link href="/login">Login</Link>
-				</li>
-				<li>
-					<Link href="/register">Register</Link>
-				</li>
-				<li>
-					<Link href={`/profile/${exampleUserId}`}>My Profile</Link>
-				</li>
 				<li>
 					<Link href="/explore">Explore</Link>
 				</li>
