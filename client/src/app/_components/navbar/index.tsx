7ca7e243e64e60f3b62bb4b08d0b2113998dc30b
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-03-22 22:24:57 +0100

    feat: basic nav styling

--- client/src/app/_components/navbar/index.tsx
+++ client/src/app/_components/navbar/index.tsx
@@ -8,19 +8,17 @@ export const Navbar = () => {
 	const exampleUserId = '69e8f4d1';
 
 	return (
-		<header>
-			<ul>
-				<li>
-					<Link href="/">Sliced</Link>
-				</li>
-				<li>
-					<Link href="/login">Login</Link> | <Link href="/register">Register</Link>
-				</li>
-				<li>
-					<Link href={`/profile/${exampleUserId}`}>My Profile</Link>
-				</li>
-			</ul>
+		<header className="w-100 flex justify-between items-center py-3 px-32 bg-yellow-500">
 			<WalletButton />
+			<Link href="/">
+				<h1 className="text-2xl">Sliced</h1>
+			</Link>
+			<div className="flex">
+				<div className="me-5">
+					<Link href="/login">Login</Link> | <Link href="/register">Register</Link>
+				</div>
+				<Link href={`/profile/${exampleUserId}`}>My Profile</Link>
+			</div>
 		</header>
 	);
 };
