b8221a4e3526bf472883ada8fe18398a25540f4f
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-03-29 16:00:13 +0100

    feat: login page per wireframe

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -6,76 +6,84 @@ import { TextInput } from '~/app/_components/ui/text-input';
 
 export default function Login() {
 	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
+	const [rememberMe, setRememberMe] = useState(false);
 
-	const credentials = {
+	type LoginFormData = {
+		email: string;
+		rememberMe: boolean;
+	};
+
+	const formData: LoginFormData = {
 		email,
-		password,
+		rememberMe,
 	};
 
 	const handleEmail = (e: { target: { value: SetStateAction<string> } }) => {
 		setEmail(e.target.value);
 	};
 
-	const handlePassword = (e: { target: { value: SetStateAction<string> } }) => {
-		setPassword(e.target.value);
+	const handleRememberMe = () => {
+		setRememberMe(!rememberMe);
 	};
 
-	const handleSubmit = () => {
-		alert(`Email: ${credentials.email}\nPassword: ${credentials.password}`);
+	const handleLogin = () => {
+		alert(`Email: ${formData.email}\nRemember me: ${formData.rememberMe}`);
+	};
+
+	const handleConnectWallet = () => {
+		alert('Connect with wallet');
 	};
 
 	return (
-		<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-			<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-				{/* Small logo can go here
-				<img
-					className="mx-auto h-10 w-auto"
-					src=""
-					alt="Sliced"
-				/> */}
-				<h2 className="mt-10 text-center text-2xl font-bold">Log in to your account</h2>
+		<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 m-auto w-96">
+			<div className="">
+				<h2 className="mb-6 text-center text-2xl font-bold">Sign in to Slice</h2>
 			</div>
 
-			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-				<form className="space-y-6" action="#" method="POST">
-					<div>
-						<TextInput
-							label="Email address"
-							id="email"
-							name="email"
-							type="email"
-							autoComplete="email"
-							required={true}
-							onChange={handleEmail}
+			<BasicButton type="primary" onClick={handleConnectWallet}>
+				Connect Wallet
+			</BasicButton>
+
+			<div className="w-100 h-3 border-b text-center mt-4 mb-5">
+				<span className="w-100 px-3 bg-white">or</span>
+			</div>
+
+			<form className="flex flex-col justify-center space-y-6" action="#" method="POST">
+				<div>
+					<TextInput
+						id="email"
+						name="email"
+						type="email"
+						placeholder="Email Address"
+						autoComplete="email"
+						required={true}
+						onChange={handleEmail}
+					/>
+					<div className="flex items-center mt-2">
+						<input
+							className="me-2 hover:cursor-pointer"
+							id="remember-me"
+							type="checkbox"
+							checked={rememberMe}
+							onChange={handleRememberMe}
 						/>
+						<label className="text-sm hover:cursor-pointer" htmlFor="remember-me">
+							Remember me
+						</label>
 					</div>
-
-					<div>
-						<TextInput
-							label="Password"
-							link={{ text: 'Forgot password?', href: '#' }}
-							id="password"
-							name="password"
-							type="password"
-							autoComplete="current-password"
-							required={true}
-							onChange={handlePassword}
-						/>
-					</div>
-
-					<div className="flex justify-center">
-						<BasicButton type="primary" onClick={handleSubmit}>
-							Login
-						</BasicButton>
-					</div>
-				</form>
-
-				<p className="mt-10 text-center text-sm text-gray-500">Don&#39;t have an account yet?</p>
-				<div className="text-sm flex justify-center">
-					<InlineLink href="/register">Register now</InlineLink>
 				</div>
-			</div>
+
+				<BasicButton type="primary" onClick={handleLogin}>
+					Sign In
+				</BasicButton>
+			</form>
+
+			<p className="mt-6 text-center text-sm text-gray-500">
+				Don&#39;t have an account?{' '}
+				<span>
+					<InlineLink href="/register">Create an account</InlineLink>
+				</span>
+			</p>
 		</div>
 	);
 }
