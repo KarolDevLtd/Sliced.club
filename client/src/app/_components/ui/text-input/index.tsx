f8fd4dc22bdc55700577dd7025a102360be71469
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-03-29 12:05:25 +0100

    feat: expand textinput component and implement

--- client/src/app/_components/ui/text-input/index.tsx
+++ client/src/app/_components/ui/text-input/index.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
+import { InlineLink } from '../inline-link';
 
 type TextInputProps = {
-	label: string;
-	placeholder: string;
+	label?: string;
+	link?: {
+		text: string;
+		external?: boolean;
+		href: string;
+	};
+	id: string;
+	name: string;
+	// Feel free to add more types to this enum as we need them
+	type: 'text' | 'email' | 'password';
+	autoComplete?: string;
+	placeholder?: string;
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	onChange?: (e: any) => any;
 	disabled?: boolean;
+	required?: boolean;
 };
 
-export const TextInput = ({ label, placeholder, onChange, disabled }: TextInputProps) => {
+export const TextInput = ({
+	label,
+	link,
+	id,
+	name,
+	type,
+	autoComplete,
+	placeholder,
+	onChange,
+	disabled,
+	required = false,
+}: TextInputProps) => {
 	return (
-		<div className="m-2">
-			{label ? <label>{label}</label> : null}
-			<input
-				type="text"
-				placeholder={placeholder}
-				onChange={onChange}
-				disabled={disabled}
-				className="w-full border-solid border-2 border-indigo-600"
-			></input>
+		<div>
+			<div className="flex items-center justify-between">
+				{label ? (
+					<label htmlFor={id} className="block text-sm font-medium leading-6 text-gray-900">
+						{label}
+					</label>
+				) : null}
+				{link ? (
+					<div className="text-sm">
+						<InlineLink href={link.href} external={link.external}>
+							{link.text}
+						</InlineLink>
+					</div>
+				) : null}
+			</div>
+			<div className="mt-1">
+				<input
+					className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+					id={id}
+					name={name}
+					type={type}
+					autoComplete={autoComplete}
+					placeholder={placeholder}
+					onChange={onChange}
+					disabled={disabled}
+					required={required}
+				></input>
+			</div>
 		</div>
 	);
 };
