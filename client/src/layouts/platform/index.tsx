3005ca05ede371f25824ce4bc67cf149e637dda5
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-04-05 23:11:35 +0200

    feat: move toast container to root layout

--- client/src/layouts/platform/index.tsx
+++ client/src/layouts/platform/index.tsx
@@ -1,35 +1,18 @@
-import { Bounce, ToastContainer } from 'react-toastify';
+import { PlatformSidebar } from '~/app/_components/global/platform-sidebar';
 import { UserStoreStatus } from '~/app/_components/temp-user-store-status';
 import Providers from '~/providers';
+import RootLayout from '..';
 
 export default function PlatformLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<Providers>
-			<UserStoreStatus />
-			<ToastContainer
-				position="bottom-right"
-				autoClose={5000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme="light"
-				transition={Bounce}
-			/>
-			<div className="container pt-28 pb-6 min-h-screen flex">
-				<div className="flex flex-col w-1/4">
-					<p>a</p>
-					<p>b</p>
-					<p>c</p>
-					<p>d</p>
-					<p>e</p>
-					<p>f</p>
+		<RootLayout>
+			<Providers>
+				<UserStoreStatus />
+				<div className="min-h-screen max-h-screen flex">
+					<PlatformSidebar />
+					<main className="flex-1 p-6">{children}</main>
 				</div>
-				<div>{children}</div>
-			</div>
-		</Providers>
+			</Providers>
+		</RootLayout>
 	);
 }
