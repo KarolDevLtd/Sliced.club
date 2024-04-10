3de9c96066b04bca0a759f74b579075896f95edf
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-04-10 22:12:12 +0200

    feat: migrate dashboard to index route

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,36 +1,27 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { useEffect } from 'react';
+
 import { useRouter } from 'next/router';
 
 import { toast } from 'react-toastify';
 
-import { InlineLink } from '~/app/_components/ui/inline-link';
-import DefaultLayout from '~/layouts/default';
+import PlatformLayout from '~/layouts/platform';
 
-import useStore from '~/stores/utils/useStore';
-import { useUserStore } from '~/providers/store-providers/userStoreProvider';
-import { type UserState } from '~/stores/userStore';
-import { partialUtil } from 'node_modules/zod/lib/helpers/partialUtil';
-
-export default function Home() {
+export default function Dashboard() {
 	const router = useRouter();
-	const isLoggedIn = useStore(useUserStore, (state: UserState) => state.isLoggedIn);
 
 	useEffect(() => {
-		if (router.query.logout === 'success') toast.success('Logged out successfully');
-	}, [router.query.logout]);
+		if (router.query.login === 'success') toast.success('Logged in successfully');
+		if (router.query.register === 'success') toast.success('Registered successfully');
+		if (router.query.login === 'wallet') toast.success('Logged in with wallet successfully');
+	}, [router.query.login, router.query.register]);
 
 	return (
 		<div>
-			<ul>
-				<li>{isLoggedIn ? <h1>Sliced</h1> : <p>Log in to go to platform</p>}</li>
-			</ul>
+			<h1>Hello (USER)!</h1>
 		</div>
 	);
 }
 
-Home.getLayout = function getLayout(page) {
-	return <DefaultLayout>{page}</DefaultLayout>;
+Dashboard.getLayout = function getLayout(page) {
+	return <PlatformLayout>{page}</PlatformLayout>;
 };
