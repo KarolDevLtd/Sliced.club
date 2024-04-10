a8b4dd86d15f52d3c2fa56840ccb8352c4d8b611
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2024-04-10 09:49:17 +0100

    ZoomableImage component

---
+++ client/src/app/_components/ui/zoomable-image/index.tsx
@@ -0,0 +1,26 @@
+import { useCallback, useState } from 'react';
+import { Controlled as ControlledZoom } from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
+import Image from 'next/image';
+
+type ZoomableImageProps = {
+	source: string;
+	width: number;
+	height: number;
+	alt: string;
+};
+
+const ZoomableImage = ({ source, width, height, alt }: ZoomableImageProps) => {
+	const [isZoomed, setIsZoomed] = useState<boolean>(false);
+
+	const handleZoomChange = useCallback((shouldZoom: boolean | ((prevState: boolean) => boolean)) => {
+		setIsZoomed(shouldZoom);
+	}, []);
+
+	return (
+		<ControlledZoom isZoomed={isZoomed} onZoomChange={handleZoomChange}>
+			<Image src={source} width={width} height={height} alt={alt} />
+		</ControlledZoom>
+	);
+};
+export default ZoomableImage;
