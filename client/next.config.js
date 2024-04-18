94ccb604c66ecf63375ba4bd0d1fd0e976fd5c7e
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2024-04-18 15:59:23 +0100

    feat/changes for basic minting

--- client/next.config.js
+++ client/next.config.js
@@ -2,9 +2,29 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+	// To enable o1js for the web, we must set the COOP and COEP headers.
+	// See here for more information: https://docs.minaprotocol.com/zkapps/how-to-write-a-zkapp-ui#enabling-coop-and-coep-headers
+	async headers() {
+		return [
+			{
+				source: '/(.*)',
+				headers: [
+					{
+						key: 'Cross-Origin-Opener-Policy',
+						value: 'same-origin',
+					},
+					{
+						key: 'Cross-Origin-Embedder-Policy',
+						value: 'require-corp',
+					},
+				],
+			},
+		];
+	},
+};
 
 export default config;
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { useEffect } from 'react';
 
 import { useRouter } from 'next/router';
@@ -5,6 +11,8 @@ import { useRouter } from 'next/router';
 import { toast } from 'react-toastify';
 
 import PlatformLayout from '~/layouts/platform';
+import { BasicButton } from '~/app/_components/ui/basic-button';
+import { Field, Mina, PrivateKey, PublicKey, AccountUpdate, UInt32, UInt64, Signature, fetchAccount } from 'o1js';
 
 export default function Dashboard() {
 	const router = useRouter();
@@ -15,9 +23,65 @@ export default function Dashboard() {
 		if (router.query.login === 'wallet') toast.success('Logged in with wallet successfully');
 	}, [router.query.login, router.query.register]);
 
+	const handleClick = async () => {
+		try {
+			const mina = (window as any).mina;
+			console.log('Before import');
+
+			const { FungibleToken } = await import('../../../contracts/build/src/token/FungibleToken');
+			console.log('After import');
+
+			const publicKeyBase58: string = (await mina.requestAccounts())[0];
+			const publicKey = PublicKey.fromBase58(publicKeyBase58);
+			// This is the public key of the deployed zkapp you want to interact with.
+			const zkAppAddress = PrivateKey.random();
+
+			console.log('Before compile');
+
+			// await FungibleToken.compile().then(() => {
+			// 	console.log('complete');
+			// });
+
+			console.log('Before fetch account');
+
+			await fetchAccount({ publicKey: publicKey });
+			await fetchAccount({ publicKey: zkAppAddress.toPublicKey() });
+
+			console.log('Before transaction');
+
+			const tx = await Mina.transaction(async () => {
+				const YourSmartContractInstance = new FungibleToken(zkAppAddress.toPublicKey());
+				await YourSmartContractInstance.mint(publicKey, new UInt64(100));
+			});
+
+			console.log('After transaction');
+
+			await tx.prove();
+
+			console.log('After proof');
+
+			const { hash } = await window.mina.sendTransaction({
+				transaction: tx.toJSON(),
+				feePayer: {
+					fee: '',
+					memo: 'zk',
+				},
+			});
+
+			console.log(hash);
+		} catch (err) {
+			// You may want to show the error message in your UI to the user if the transaction fails.
+			console.log(err.message);
+		}
+	};
+
 	return (
 		<div>
 			<h1>Hello (USER)!</h1>
+
+			<BasicButton type={'primary'} onClick={handleClick}>
+				TEST MINT
+			</BasicButton>
 		</div>
 	);
 }
