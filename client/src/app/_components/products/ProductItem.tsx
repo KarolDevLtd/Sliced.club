9d7fa587027ad12bbd81e5efba67f7c37fe1217a
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-04-21 22:02:46 +0200

    feat: create ProductList and groups module

--- client/src/app/_components/products/ProductItem.tsx
+++ client/src/app/_components/products/ProductItem.tsx
@@ -1,47 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FaUserGroup } from 'react-icons/fa6';
 import { BasicButton } from '../ui/basic-button';
-
-export type Product = {
-	title: string;
-	groupOrganiser: string;
-	category: string;
-	groupMembers: number;
-	completed: number;
-};
+import { type Product } from '~/types/product-types';
+import { BasicModal } from '../ui/basic-modal';
 
 type ProductItemProps = {
 	product: Product;
 };
 
 const ProductItem = ({ product }: ProductItemProps) => {
-	const completedPercentage = (decimal: number | undefined) => {
-		return `${decimal ? decimal * 100 : 0}%`;
+	const [displayModal, setDisplayModal] = useState(false);
+
+	const toggleModal = () => {
+		setDisplayModal(!displayModal);
+	};
+
+	const completedPercentage = () => {
+		return product.itemsReceived ? `${Math.round((product.itemsReceived / product.groupMembers) * 100)}%` : 0;
 	};
 
 	return (
-		<div className="grid grid-cols-10 gap-4 p-2 bg-light-grey min-w-full min-h-[90px] rounded-md">
-			<div className="col-span-2 w-12 min-h-full min-w-full bg-medium-grey rounded"></div>
-			<div className="col-span-2 flex flex-col justify-center">
-				<p className="font-bold">{product?.title}</p>
-				<p>{product?.groupOrganiser}</p>
+		<>
+			<div className="grid grid-cols-10 gap-4 p-2 bg-light-grey min-w-full min-h-[90px] rounded-md">
+				<div className="col-span-2 w-12 min-h-full min-w-full bg-medium-grey rounded"></div>
+				<div className="col-span-2 flex flex-col justify-center">
+					<p className="font-bold">{product?.title}</p>
+					<p>{product?.groupOrganiser}</p>
+				</div>
+				<div className="col-span-2 flex items-center">
+					<p>{product?.category}</p>
+				</div>
+				<div className="col-span-1 flex items-center gap-1">
+					<FaUserGroup />
+					<p>{product?.groupMembers}</p>
+				</div>
+				<div className="col-span-1 flex items-center">
+					{product?.itemsReceived ? <p>{completedPercentage()}</p> : /* Progress bar */ null}
+				</div>
+				<div className="col-span-2 flex items-center">
+					<BasicButton type="secondary" onClick={toggleModal}>
+						View details
+					</BasicButton>
+				</div>
 			</div>
-			<div className="col-span-2 flex items-center">
-				<p>{product?.category}</p>
-			</div>
-			<div className="col-span-1 flex items-center gap-1">
-				<FaUserGroup />
-				<p>{product?.groupMembers}</p>
-			</div>
-			<div className="col-span-1 flex items-center">
-				<p>{completedPercentage(product?.completed)}</p>
-				{/* Progress bar */}
-			</div>
-			<div className="col-span-2 flex items-center">
-				<BasicButton type="secondary">View details</BasicButton>
-			</div>
-		</div>
+			<BasicModal
+				isOpen={displayModal}
+				onClose={toggleModal}
+				header={<h2 className="text-xl font-semibold">Item Details</h2>}
+				content={
+					<div>
+						<div className="flex items-center gap-1">
+							<strong>Product name:</strong> <p>{product.title}</p>
+						</div>
+						<div className="flex items-center gap-1">
+							<strong>Group Organiser:</strong> <p>{product.groupOrganiser}</p>
+						</div>
+						<div className="flex items-center gap-1">
+							<strong>Price:</strong> <p>{product.price}</p>
+						</div>
+						<div className="flex items-center gap-1">
+							<strong>Group members:</strong> <p>{product.groupMembers}</p>
+						</div>
+						<div className="flex items-center gap-1">
+							<strong>Items received:</strong> <p>{product.itemsReceived}</p>
+						</div>
+					</div>
+				}
+			/>
+		</>
 	);
 };
 
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -7,7 +7,8 @@ import { toast } from 'react-toastify';
 import PlatformLayout from '~/layouts/platform';
 import { DashboardHeader } from '~/app/_components/dashboard/DashboardHeader';
 import Carousel from '~/app/_components/ui/Carousel';
-import MyProducts from '~/app/_components/products/MyProducts';
+import ProductList from '~/app/_components/products/ProductList';
+import { type Product } from '~/types/product-types';
 
 export default function Dashboard() {
 	const router = useRouter();
@@ -17,6 +18,56 @@ export default function Dashboard() {
 		if (router.query.register === 'success') toast.success('Registered successfully');
 		if (router.query.login === 'wallet') toast.success('Logged in with wallet successfully');
 	}, [router.query.login, router.query.register]);
+
+	const myProducts = [
+		{
+			title: 'Xbox Series X|S',
+			groupOrganiser: 'Microsoft',
+			price: 475,
+			category: 'Gaming Consoles',
+			groupMembers: 26,
+			itemsReceived: 14,
+		},
+		{
+			title: 'HERO 12 Black',
+			groupOrganiser: 'GoPro',
+			price: 429.99,
+			category: 'Cameras',
+			groupMembers: 41,
+			itemsReceived: 36,
+		},
+	] as Product[];
+
+	const availableGroups = [
+		{
+			title: 'iPhone 15',
+			groupOrganiser: 'Apple',
+			price: 1149.99,
+			category: 'Mobile Phones',
+			groupMembers: 119,
+		},
+		{
+			title: 'LG TV OLED 2376',
+			groupOrganiser: 'LG',
+			price: 999.99,
+			category: 'Televisions',
+			groupMembers: 69,
+		},
+		{
+			title: 'Birkin 25',
+			groupOrganiser: 'Hermes',
+			price: 11400,
+			category: 'Handbags',
+			groupMembers: 21,
+		},
+		{
+			title: 'Cyrusher Kommoda Bike',
+			groupOrganiser: 'Cyrusher',
+			price: 1699,
+			category: 'Electric Bikes',
+			groupMembers: 12,
+		},
+	] as Product[];
 
 	return (
 		<div className="flex flex-col gap-4 min-h-full max-h-full">
@@ -52,9 +103,10 @@ export default function Dashboard() {
 				</div>
 				<div className="row-span-2 grid grid-cols-3 gap-2">
 					<div className="col-span-2">
-						<MyProducts />
+						<ProductList heading="My Products" products={myProducts} />
+						<ProductList heading="Available Groups" products={availableGroups} />
 					</div>
-					<div className="col-span-1 bg-light-grey rounded-md"></div>
+					<div className="col-span-1 bg-light-grey rounded-md mb-4"></div>
 				</div>
 			</div>
 		</div>
