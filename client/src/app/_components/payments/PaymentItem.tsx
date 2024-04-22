cda53e002814f84fb90617d3e25cc7f0cbb1c572
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-04-22 21:18:29 +0200

    feat: basic payment item styling

--- client/src/app/_components/payments/PaymentItem.tsx
+++ client/src/app/_components/payments/PaymentItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { formatCurrency } from '~/helpers/currency-helper';
+import { formatDate } from '~/helpers/date-helper';
 import { type Payment } from '~/types/payment-types';
 
 type PaymentItemProps = {
@@ -9,14 +10,15 @@ type PaymentItemProps = {
 const PaymentItem = ({ payment }: PaymentItemProps) => {
 	return (
 		<div className="flex gap-4 items-center p-2 bg-light-grey min-w-full min-h-[90px] rounded-md">
-			<div className="h-12 w-12 bg-purple-light rounded-md"></div>
+			<div className="h-12 w-12 bg-medium-grey rounded-md"></div>
 			<div>
 				<p>
-					<span>{formatCurrency(payment.amountDue)}</span>{' '}
-					<span>{payment.nextPaymentDue.toDateString()}</span>
+					<span className="font-bold">{formatCurrency(payment.amountDue)}</span>{' '}
+					<span className="text-sm text-dark-grey">Due {formatDate(payment.nextPaymentDue)}</span>
 				</p>
 				<p>
-					<span>{payment.product.groupOrganiser}</span> - <span>{payment.product.title}</span>
+					<span className="font-bold">{payment.product.title}</span>{' '}
+					<span className="text-sm text-dark-grey">{payment.product.groupOrganiser}</span>
 				</p>
 			</div>
 		</div>
