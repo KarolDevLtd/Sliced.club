0991956543e031a904665cb7481978edd5f6d0fb
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-04-29 23:29:39 +0200

    feat: add header quicklink wireframes

--- client/src/app/_components/ui/PageHeader.tsx
+++ client/src/app/_components/ui/PageHeader.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import Image from 'next/image';
+
+import { FaBell } from 'react-icons/fa';
+import { FaCaretDown } from 'react-icons/fa';
 
 type PageHeaderProps = {
 	text: string;
@@ -7,9 +11,24 @@ type PageHeaderProps = {
 
 const PageHeader = ({ text, subtext }: PageHeaderProps) => {
 	return (
-		<div>
-			<h1 className="text-4xl">{text}</h1>
-			{subtext ? <p className="text-xs text-dark-grey">{subtext}</p> : null}
+		<div className="flex justify-between items-center">
+			<div>
+				<h1 className="text-4xl">{text}</h1>
+				{subtext ? <p className="text-xs text-dark-grey">{subtext}</p> : null}
+			</div>
+			<div className="flex items-center gap-3">
+				<div className=" flex justify-center items-center border rounded-md min-h-[2.25rem] min-w-[2.25rem]">
+					<FaBell />
+				</div>
+				<div className="flex items-center bg-black border rounded-md min-h-[2.25rem] min-w-[2.25rem] overflow-hidden">
+					<div className="bg-white rounded-r-md min-h-[2.25rem] min-w-[2.25rem]">
+						<Image src={''} alt={''} />
+					</div>
+					<div className="min-w-[0.25rem]">
+						<FaCaretDown className="h-[12px] w-[12px] text-white" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
