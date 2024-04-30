08c41e4a572b7ce69b3827f9256cb7d502a6da9b
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2024-04-30 06:33:09 +0100

    fix:reinstate the use of ProductItem component for listing products

--- client/src/app/_components/products/ProductList.tsx
+++ client/src/app/_components/products/ProductList.tsx
@@ -6,12 +6,10 @@ import ProductItem from './ProductItem';
 import { type Product } from '~/types/product-types';
 import { api } from '~/trpc/react';
 import { useWallet } from '~/providers/walletprovider';
-import { preventActionWalletNotConnected } from '~/helpers/user-helper';
-
 import useStore from '~/stores/utils/useStore';
 import { useUserStore } from '~/providers/store-providers/userStoreProvider';
 import { type UserState } from '~/stores/userStore';
-import { FirebaseProductModel } from '~/models/firebase-product-model';
+import { type FirebaseProductModel } from '~/models/firebase-product-model';
 
 type ProductListProps = {
 	heading?: string;
@@ -20,11 +18,6 @@ type ProductListProps = {
 
 const ProductList = ({ heading }: ProductListProps) => {
 	const { isConnected, walletAddress } = useWallet();
-
-	const isLoggedIn = useStore(useUserStore, (state: UserState) => state.isLoggedIn);
-	const walletConnected = useStore(useUserStore, (state: UserState) => state.walletConnected);
-
-	// if (preventActionWalletNotConnected(walletConnected, 'Connect a wallet to post')) return;
 
 	const {
 		data: productData,
@@ -58,8 +51,7 @@ const ProductList = ({ heading }: ProductListProps) => {
 			{heading ? <h2 className="text-2xl">{heading}</h2> : null}
 			{products ? (
 				products.map((product, index) => {
-					return <div key={index}>{product.productHash}</div>;
-					// <ProductItem key={index} product={product} />;
+					return <ProductItem key={index} currentProduct={product.productHash} />;
 				})
 			) : (
 				<p>No products found.</p>
