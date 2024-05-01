de953562c35716d68cdfa0954cf7a09b1d51dce6
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2024-05-01 08:02:37 +0100

    feat: :lipstick: add label to select-option & display key functionality
    display key functionality can be used to specify which of the options is to be display on the UI

--- client/src/app/_components/ui/select-option/index.tsx
+++ client/src/app/_components/ui/select-option/index.tsx
@@ -6,28 +6,30 @@ import React from 'react';
 type SelectOptionProps = {
 	id: string;
 	name: string;
+	label?: string;
 	placeholder?: string;
 	disabled?: boolean;
 	required?: boolean;
 	options: {
 		value: string;
 		name: string;
+		symbol?: string;
 	}[];
 	defaultValue?: string;
 	value?: string;
 	onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
-
-	// React Hook Form Props
 	validationSchema?: {
 		required?: string;
 	};
 	register?: any;
 	errors?: any;
+	displayKey?: 'value' | 'name' | 'symbol'; // Allow 'value', 'name', or 'symbol'
 };
 
 export const SelectOption = ({
 	id,
 	name,
+	label,
 	placeholder,
 	disabled,
 	required = false,
@@ -35,20 +37,25 @@ export const SelectOption = ({
 	defaultValue,
 	value,
 	onChange,
-
-	// React Hook Form Props
+	displayKey = 'name', // Default to 'name'
 	validationSchema,
 	register = () => [],
 	errors,
 }: SelectOptionProps) => {
 	const optionList = options.map((option, index) => (
 		<option key={index} value={option.value}>
-			{option.name}
+			{option[displayKey]} {/* Dynamically select the display key */}
 		</option>
 	));
 
 	return (
 		<div>
+			{label ? (
+				<label htmlFor={id} className="block text-sm font-medium leading-6 text-gray-900">
+					{label}
+					{required && '*'}
+				</label>
+			) : null}
 			<select
 				className="mt-1 block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
 				id={id}
