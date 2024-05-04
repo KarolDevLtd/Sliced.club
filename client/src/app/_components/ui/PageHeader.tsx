cc9fabe74e58decc97e9c2e4b8c13237e50d5fbf
Liam Best <59018845+lbbest@users.noreply.github.com>
2024-05-04 18:04:11 +0200

    feat: hide quick links prop

--- client/src/app/_components/ui/PageHeader.tsx
+++ client/src/app/_components/ui/PageHeader.tsx
@@ -7,28 +7,33 @@ import { FaCaretDown } from 'react-icons/fa';
 type PageHeaderProps = {
 	text: string;
 	subtext?: string;
+	hideQuickLinks?: boolean;
 };
 
-const PageHeader = ({ text, subtext }: PageHeaderProps) => {
+const PageHeader = ({ text, subtext, hideQuickLinks = false }: PageHeaderProps) => {
 	return (
-		<div className="flex justify-between items-center">
+		<div className="flex justify-between items-center min-h-[56px]">
 			<div>
 				<h1 className="text-4xl">{text}</h1>
-				{subtext ? <p className="text-xs text-dark-grey">{subtext}</p> : null}
-			</div>
-			<div className="flex items-center gap-3">
-				<div className=" flex justify-center items-center border rounded-md min-h-[2.25rem] min-w-[2.25rem]">
-					<FaBell />
-				</div>
-				<div className="flex items-center bg-black border rounded-md min-h-[2.25rem] min-w-[2.25rem] overflow-hidden">
-					<div className="bg-white rounded-r-md min-h-[2.25rem] min-w-[2.25rem]">
-						<Image src={''} alt={''} />
-					</div>
-					<div className="min-w-[0.25rem]">
-						<FaCaretDown className="h-[12px] w-[12px] text-white" />
-					</div>
+				<div className="min-h-[16px]">
+					{subtext ? <p className="text-xs text-dark-grey">{subtext}</p> : null}
 				</div>
 			</div>
+			{!hideQuickLinks ? (
+				<div className="flex items-center gap-3">
+					<div className=" flex justify-center items-center border rounded-md min-h-[2.25rem] min-w-[2.25rem]">
+						<FaBell />
+					</div>
+					<div className="flex items-center bg-black border rounded-md min-h-[2.25rem] min-w-[2.25rem] overflow-hidden">
+						<div className="bg-white rounded-r-md min-h-[2.25rem] min-w-[2.25rem]">
+							<Image src={''} alt={''} />
+						</div>
+						<div className="min-w-[0.25rem]">
+							<FaCaretDown className="h-[12px] w-[12px] text-white" />
+						</div>
+					</div>
+				</div>
+			) : null}
 		</div>
 	);
 };
