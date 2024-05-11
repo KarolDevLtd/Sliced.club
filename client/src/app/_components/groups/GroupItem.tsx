9658a0278db0eb3e144f3a9f53cd7155b9743a8f
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2024-05-11 07:38:52 +0100

    feat: :lipstick: update group item component closer to figma design

--- client/src/app/_components/groups/GroupItem.tsx
+++ client/src/app/_components/groups/GroupItem.tsx
@@ -11,6 +11,12 @@ import { api } from '~/trpc/react';
 import BasicModal from '../ui/BasicModal';
 import { toast } from 'react-toastify';
 import { type FirebaseGroupModel } from '~/models/firebase/firebase-group-model';
+import ZoomableImage from '../ui/ZoomableImage';
+import { type IPFSProductModel } from '~/models/ipfs/ipfs-product-model';
+import { fetchImageData } from '~/helpers/image-helper';
+import { IoPeople } from 'react-icons/io5';
+import InlineLink from '../ui/InlineLink';
+import BasicButton from '../ui/BasicButton';
 
 type GroupItemProps = {
 	firebaseGroup: FirebaseGroupModel;
@@ -18,19 +24,18 @@ type GroupItemProps = {
 
 const GroupItem = ({ firebaseGroup }: GroupItemProps) => {
 	const { data: groupData } = api.PinataGroup.getGroup.useQuery({ hash: firebaseGroup.groupHash });
+	const { data: productData } = api.PinataProduct.getProduct.useQuery({ hash: groupData?.group.productHash });
 	const [displayModal, setDisplayModal] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
 	const [group, setGroup] = useState<IPFSGroupModel>();
+	const [product, setProduct] = useState<IPFSProductModel>();
+	const [hasImage, setHasImage] = useState<boolean>(false);
+	const [imageData, setImageData] = useState<string[]>([]);
+	const [imageError, setImageError] = useState(false);
 
 	const toggleModal = () => {
 		setDisplayModal(!displayModal);
 	};
-
-	// const completedRatio = product?.itemsReceived ? (product.itemsReceived / product.groupMembers) * 100 : 0;
-
-	// const completedPercentage = () => {
-	// 	return product.itemsReceived ? `${Math.round(completedRatio)}%` : 0;
-	// };
 
 	const handleClick = (e: Event | undefined) => {
 		//At this point we have all group information from firebase and IPFS
@@ -41,7 +46,7 @@ const GroupItem = ({ firebaseGroup }: GroupItemProps) => {
 				groupName: firebaseGroup.name,
 				groupHash: firebaseGroup.groupHash,
 				productHash: group?.productHash,
-			}, // Pass firebaseGroup data as query parameter
+			},
 		});
 		e?.stopPropagation();
 	};
@@ -49,25 +54,30 @@ const GroupItem = ({ firebaseGroup }: GroupItemProps) => {
 	// Get data from Firebase
 	const fetchInfo = useCallback(async () => {
 		setIsLoading(true);
+		console.log(productData);
 		try {
 			if (groupData) {
 				const currGroup = groupData.group as IPFSGroupModel;
 				setGroup(currGroup);
 			}
+			if (productData) {
+				const currProd = productData.product as IPFSProductModel;
+				setProduct(productData.product);
+				await fetchImageData(currProd, setHasImage, setImageData, setImageError);
+			}
 		} catch (err) {
 			console.log(err);
-			toast.error('Error fetching one or more images');
+			toast.error('Error fetching group item info');
 		} finally {
 			setIsLoading(false);
 		}
-	}, [groupData]);
+	}, [groupData, productData]);
 
 	useEffect(() => {
 		void fetchInfo();
 	}, [fetchInfo, group]);
 
 	return (
-		// <div>{currentProduct}</div>
 		<>
 			{isLoading ? (
 				'Loading...'
@@ -76,79 +86,32 @@ const GroupItem = ({ firebaseGroup }: GroupItemProps) => {
 				<div
 					className="grid grid-cols-10 gap-4 p-4 bg-light-grey min-w-full min-h-[90px] rounded-md border border-[transparent] hover:border-black hover:cursor-pointer"
 					// @ts-ignore
-					onClick={(e) => handleClick(e)}
+					// onClick={(e) => handleClick(e)}
 				>
-					{/* <div className="col-span-2 max-w-[120px] min-h-full bg-medium-grey rounded">
+					<div className="col-span-1 flex flex-col justify-center">
 						{hasImage ? (
 							<ZoomableImage source={imageData[0] ?? null} width={100} height={100} alt={'image'} />
 						) : null}
-					</div> */}
-					<div className="col-span-2 flex flex-col justify-center">
-						<strong>Group name:</strong> <p>{group?.name}</p>
-						<strong>Creator ID:</strong> <p>{firebaseGroup.creatorKey}</p>
-						<strong>Price:</strong>{' '}
-						<p>
-							{group?.currency}
-							{group?.price}
-						</p>
-						<strong>Duration:</strong> <p>{group?.duration}</p>
-						<strong>Participants:</strong> <p>{group?.participants}</p>
-						<strong>Product Hash:</strong> <p>{group?.productHash}</p>
-						{/* <p className="text-sm text-dark-grey">{creatorId}</p> */}
+					</div>
+					<div className="flex flex-col col-span-3 items-center justify-center">
+						<strong>{group?.name}</strong>
+						<strong>{product?.name}</strong>
 					</div>
 
-					{/* <div className="col-span-1 flex items-center gap-1">
-					<FaUserGroup />
-					<p>{product?.groupMembers}</p>
-				</div> */}
-					{/* <div className="col-span-1 flex items-center">
-					{product?.itemsReceived ? (
-						<div>
-							<p>{completedPercentage()}</p>
-							<ProgressBar progress={completedRatio} />
-						</div>
-					) : null}
-				</div> */}
-					{/* <div className="col-span-2 flex items-center">
-					<BasicButton type="secondary" onClick={(e) => handleClick(e)}>
-						View details
-					</BasicButton>
-				</div> */}
-					<BasicModal
-						isOpen={displayModal}
-						onClose={toggleModal}
-						header={<h2 className="text-xl font-semibold">Group Details</h2>}
-						content={
-							<div>
-								<div className="flex items-center gap-1">
-									<strong>Group name:</strong> <p>{group?.name}</p>
-								</div>
-								<div className="flex items-center gap-1">
-									<strong>Organiser:</strong> <p>{groupData?.group?.creatorId}</p>
-									<strong>:</strong> <p>{group?.country}</p>
-								</div>
-								<div className="flex items-center gap-1">
-									<strong>Price:</strong> <p>{group?.price}</p>
-									<strong>:</strong> <p>{group?.country}</p>
-								</div>
-								<div className="flex items-center gap-1">
-									<strong>Currency:</strong> <p>{group?.currency}</p>
-								</div>
-								<div className="flex items-center gap-1">
-									<strong>Duration:</strong> <p>{group?.duration}</p>
-									<strong>:</strong> <p>{group?.country}</p>
-								</div>
-								{/* <div className="flex items-center gap-1">
-									<strong>Group members:</strong> <p>{product.groupMembers}</p>
-								</div>
-								{product.itemsReceived ? (
-									<div className="flex items-center gap-1">
-										<strong>Items received:</strong> <p>{product.itemsReceived}</p>
-									</div>
-								) : null} */}
-							</div>
-						}
-					/>
+					<div className="flex flex-col col-span-2 items-center justify-center">
+						<InlineLink href={`categories/${product?.category}`}>{product?.category}</InlineLink>
+					</div>
+
+					<div className="flex flex-row col-span-2 items-center justify-center">
+						<IoPeople />
+						<p>{group?.participants}</p>
+					</div>
+
+					<div className="flex flex-col col-span-2 items-center justify-center">
+						<BasicButton type={'secondary'} onClick={(e) => handleClick(e)}>
+							View Details
+						</BasicButton>
+					</div>
 				</div>
 			)}
 		</>
