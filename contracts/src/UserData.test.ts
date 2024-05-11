12be0c37f8e09851f1514bab0f5dbeda56e6209f
karol <karolpodufalski@gmail.com>
2024-05-11 19:34:33 +0100

    move token acc storage contracts from another repo

---
+++ contracts/src/UserData.test.ts
@@ -0,0 +1,374 @@
+import { UserData, TestContract, TokenField, Payments } from './UserData';
+import {
+  Field,
+  Mina,
+  PrivateKey,
+  PublicKey,
+  AccountUpdate,
+  VerificationKey,
+  fetchAccount,
+  Bool,
+  UInt64,
+  Transaction,
+  setGraphqlEndpoint,
+} from 'o1js';
+
+import { TestPublicKey } from 'o1js/dist/node/lib/mina/local-blockchain';
+
+let proofsEnabled = true;
+
+describe('Token account write', () => {
+  let getAccount: any;
+  let deployer: TestPublicKey,
+    sender: TestPublicKey,
+    group: TestPublicKey,
+    feePayer: TestPublicKey,
+    udPrivateKey: PrivateKey,
+    tcPrivateKey: PrivateKey,
+    ud: UserData,
+    tc: TestContract,
+    tokenKey: PrivateKey,
+    verificationKey: VerificationKey;
+
+  beforeAll(async () => {
+    console.log('yo');
+
+    if (proofsEnabled) {
+      await UserData.compile();
+      const { verificationKey: vk2 } = await TestContract.compile();
+      verificationKey = vk2;
+    }
+
+    console.log('yo');
+    const Local = await Mina.LocalBlockchain({ proofsEnabled });
+    getAccount = await Local.getAccount;
+    console.log('Started local blockchain');
+    Mina.setActiveInstance(Local);
+    console.log('Set something ');
+    deployer = await Local.testAccounts[0];
+    sender = await Local.testAccounts[1];
+    group = await Local.testAccounts[2];
+    feePayer = await Local.testAccounts[3];
+    tokenKey = PrivateKey.random();
+
+    console.log('Made keys');
+    udPrivateKey = PrivateKey.random();
+    tcPrivateKey = PrivateKey.random();
+    ud = new UserData(udPrivateKey.toPublicKey());
+    tc = new TestContract(tcPrivateKey.toPublicKey());
+    console.log('End');
+
+    const salt = await Field.random();
+    const deployTnx = await Mina.transaction(deployer, async () => {
+      AccountUpdate.fundNewAccount(deployer);
+      // ud.deploy({ group });
+      tc.deploy();
+    });
+
+    console.log('Deploying the zkApp');
+    await deployTnx.prove();
+    console.log('Deployed');
+    await deployTnx.sign([deployer.key, tcPrivateKey]).send();
+  });
+
+  // it('Increment secret', async () => {
+  //   await localDeploy(group);
+
+  //   try {
+  //     const txn1 = await Mina.transaction(sender, async () => {
+  //       zkApp.incrementSecret(Field(750));
+  //     });
+  //     await txn1.prove();
+  //     console.log('Proven increment sectre');
+
+  //     console.log('Attempting to sign by: ', senderAccount.toBase58());
+  //     console.log(
+  //       'Private key creates account: ',
+  //       senderKey.toPublicKey().toBase58()
+  //     );
+  //     await txn1.sign([sender]).send();
+  //     console.log('Signed by: ', sender.toBase58());
+  //   } catch (e: any) {
+  //     console.log(e.message.toString());
+  //   }
+  //   const num1 = zkApp.x.get();
+  //   console.log(`After first transaction value is: ${num1.toString()}`);
+  // });
+
+  xit('Calls setStateUser', async () => {
+    // await localDeploy(group);
+
+    // let tokenId = zkApp.token.id;
+    let tokenId = ud.tokenId;
+
+    try {
+      const txn1 = await Mina.transaction(sender, async () => {
+        AccountUpdate.fundNewAccount(sender);
+        ud.initialiseUserAccount(sender.key, Field(6969), tokenId);
+      });
+      await txn1.prove();
+      console.log('Proven set state in token account');
+      await txn1.sign([sender.key]).send();
+      console.log('Signed by: ', sender.toBase58());
+    } catch (e: any) {
+      console.log('Error 1: ', e.message.toString());
+    }
+
+    console.log('Initialised user account');
+
+    // Log user state at this account
+    let state = await fetchAccount({
+      publicKey: sender,
+      tokenId,
+    });
+
+    try {
+      const txn1 = await Mina.transaction(sender, async () => {
+        // AccountUpdate.fundNewAccount(senderKey);
+        ud.initialiseUserAccount(sender.key, Field(666), tokenId);
+      });
+      await txn1.prove();
+      console.log('Proven set state in token account');
+      await txn1.sign([sender.key]).send();
+      console.log('Signed by: ', sender.key.toPublicKey().toBase58());
+    } catch (e: any) {
+      console.log('Error 2: ', e.message.toString());
+    }
+
+    let state2 = getAccount(sender, tokenId);
+    // console.log('State:', state2.zkapp.appState[0].toString());
+    console.log('permissions.editState:', state2.permissions.editState);
+
+    console.log(
+      'permissions.editState:',
+      state2.permissions.editState.constant.toString()
+    );
+
+    console.log(
+      'permissions.setPermissions:',
+      state2.permissions.setPermissions.toString()
+    );
+
+    console.log(
+      'permissions.receive.constant:',
+      state2.permissions.receive.constant.toString()
+    );
+
+    console.log(
+      'permissions.receive.signatureSufficient:',
+      state2.permissions.receive.signatureSufficient.toString()
+    );
+
+    console.log(
+      'permissions.receive.signatureNecessary:',
+      state2.permissions.receive.signatureNecessary.toString()
+    );
+    // console.log('State:', state);
+  });
+
+  xit('Calls add payemnts for user', async () => {
+    // await localDeploy(group);
+
+    // let tokenId = zkApp.token.id;
+    let tokenId = ud.tokenId;
+
+    try {
+      const txn1 = await Mina.transaction(sender, async () => {
+        // await AccountUpdate.fundNewAccount(sender);
+        await ud.initialiseUserAccount(sender.key, Field(6969), tokenId);
+      });
+      await txn1.prove();
+      console.log('Proven set state in token account');
+      await txn1.sign([sender.key]).send();
+      console.log('User init signed by: ', sender.toBase58());
+    } catch (e: any) {
+      console.log('Error account creation: ', e.message.toString());
+    }
+
+    // Log one from this conbtract
+
+    // console.log('One field:', state.toString());
+
+    // // Call read token field method
+    // let paymentsFieldBefore = await zkApp.readTokenField(
+    //   sender,
+    //   tokenId,
+    //   TokenField.Payments
+    // );
+
+    // console.log(
+    //   'Payments field before payment',
+    //   paymentsFieldBefore.toString()
+    // );
+
+    //
+
+    let state2 = getAccount(sender, tokenId);
+    console.log('State2[0]:', state2);
+    console.log('start State2[0]:', state2.zkapp.appState[0].toString());
+    console.log('start State2[1]:', state2.zkapp.appState[1].toString());
+    console.log('start State2[2]:', state2.zkapp.appState[2].toString());
+
+    try {
+      const txn1 = await Mina.transaction(sender, async () => {
+        // Pay off for the month 0xw
+        await ud.paySegments(new UInt64(0));
+      });
+      await txn1.prove();
+      console.log('Proven set state in token account');
+      await txn1.sign([sender.key]).send();
+      console.log(
+        'Pay segment signed by: ',
+        sender.key.toPublicKey().toBase58()
+      );
+    } catch (e: any) {
+      console.log('Error 2: ', e.message.toString());
+    }
+
+    // state = await zkApp.one.get();
+    // console.log('[2] One field:', state.toString());
+
+    // let state2 = getAccount(sender, tokenId);
+    // // Convert to bool array
+    // let boolArr: Bool[] = await Payments.unpack(state2.zkapp.appState[0]);
+    let payments = ud.payments.get();
+    let boolArr: Bool[] = await Payments.unpack(payments);
+
+    for (let b = 0; b < 10; b++) {
+      console.log(`[0]boolArr [${b}]:  ${boolArr[b]}`);
+    }
+
+    try {
+      const txn1 = await Mina.transaction(sender, async () => {
+        // Pay off for the month 0xw
+        await ud.paySegments(new UInt64(1));
+      });
+      await txn1.prove();
+      console.log('Proven set state in token account');
+      await txn1.sign([sender.key]).send();
+      console.log(
+        'Pay segment signed by: ',
+        sender.key.toPublicKey().toBase58()
+      );
+    } catch (e: any) {
+      console.log('Error 2: ', e.message.toString());
+    }
+
+    // state = await zkApp.one.get();
+    // console.log('[3] One field:', state.toString());
+
+    // try {
+    //   const txn1 = await Mina.transaction(sender, async () => {
+    //     // Pay off for the month 0xw
+    //     await zkApp.paySegments(new UInt64(3), sender, tokenId);
+    //   });
+    //   await txn1.prove();
+    //   console.log('Proven set state in token account');
+    //   await txn1.sign([sender.key]).send();
+    //   console.log(
+    //     'Pay segment signed by: ',
+    //     sender.key.toPublicKey().toBase58()
+    //   );
+    // } catch (e: any) {
+    //   console.log('Error 2: ', e.message.toString());
+    // }
+
+    // Doesn't fetch state
+    // let paymentsField = await zkApp.readTokenField(
+    //   sender,
+    //   tokenId,
+    //   TokenField.Payments
+    // );
+    // console.log('Payments field after payment', paymentsField);
+
+    // Does fetch state
+    state2 = getAccount(sender, tokenId);
+    console.log('State2[0]:', state2);
+    console.log('State2[0]:', state2.zkapp.appState[0].toString());
+    console.log('State2[1]:', state2.zkapp.appState[1].toString());
+    console.log('State2[2]:', state2.zkapp.appState[2].toString());
+    payments = ud.payments.get();
+    boolArr = await Payments.unpack(payments);
+
+    // Convert to bool array
+    // boolArr = await Payments.unpack(state2.zkapp.appState[0]);
+
+    for (let b = 0; b < 10; b++) {
+      console.log(`boolArr [${b}]:  ${boolArr[b]}`);
+    }
+
+    for (let b = boolArr.length - 20; b < boolArr.length; b++) {
+      console.log(`boolArr [${b}]:  ${boolArr[b]}`);
+    }
+
+    // Fetch bool state from the contract
+  });
+
+  it('Proxy update', async () => {
+    // await localDeploy(group);
+
+    // // let tokenId = zkApp.token.id;
+    // let tokenId = ud.tokenId;
+
+    // try {
+    //   const txn1 = await Mina.transaction(sender, async () => {
+    //     // await AccountUpdate.fundNewAccount(sender);
+    //     await ud.initialiseUserAccount(sender.key, Field(6969), tokenId);
+    //   });
+    //   await txn1.prove();
+    //   console.log('Proven set state in token account');
+    //   await txn1.sign([sender.key]).send();
+    //   console.log('User init signed by: ', sender.toBase58());
+    // } catch (e: any) {
+    //   console.log('Error account creation: ', e.message.toString());
+    // }
+    // console.log('init done');
+
+    try {
+      const txn1 = await Mina.transaction(sender, async () => {
+        // Pay off for the month 0xw
+        // await tc.updateState(sender.key.toPublicKey(), new UInt64(1));
+        // await tc.initUser(sender.key, Field(69633339));
+        // await tc.dudd();
+        // await AccountUpdate.fundNewAccount(sender, 1);
+        await tc.initialiseUserAccount(
+          sender.key.toPublicKey(),
+          verificationKey,
+          Field(999)
+        );
+        // await tc.requireSignature();
+      });
+      await txn1.prove();
+      console.log('Proven set state in token account');
+      await txn1.sign([sender.key, tokenKey]).send();
+      console.log(
+        'Pay segment signed by: '
+        // sender.key.toPublicKey().toBase58()
+      );
+
+      let x = tc.x.get();
+      console.log('x in test: ', x.value.toString());
+    } catch (e: any) {
+      console.log('Error 2: ', e.message.toString());
+    }
+
+    console.log("Initialised user'");
+
+    try {
+      const txn1 = await Mina.transaction(sender, async () => {
+        // Pay off for the month 0xw
+        await tc.updateState(sender.key.toPublicKey(), new UInt64(1));
+        // await tc.requireSignature();
+      });
+      await txn1.prove();
+      console.log('Proven set state in token account');
+      await txn1.sign([sender.key]).send();
+      console.log(
+        'Pay segment signed by: '
+        // sender.key.toPublicKey().toBase58()
+      );
+    } catch (e: any) {
+      console.log('Error 3: ', e.message.toString());
+    }
+  });
+});
---
+++ contracts/src/UserData.ts
@@ -0,0 +1,321 @@
+import {
+  Field,
+  SmartContract,
+  TokenContract,
+  state,
+  State,
+  method,
+  Account,
+  AccountUpdate,
+  VerificationKey,
+  Experimental,
+  PrivateKey,
+  Bool,
+  UInt64,
+  PublicKey,
+  Provable,
+  Permissions,
+  DeployArgs,
+  fetchAccount,
+} from 'o1js';
+
+import { PackedBoolFactory } from './lib/packed-types/PackedBool';
+
+export class Payments extends PackedBoolFactory(250) {}
+
+export const TokenField = {
+  Payments: Field(0),
+  Compensations: Field(1),
+  Overpayments: Field(2),
+  Group: Field(3),
+};
+
+// Contract. Gets deployed
+export class TestContract extends SmartContract {
+  @state(Field) x = State<Field>();
+  async deploy() {
+    super.deploy();
+    this.account.permissions.set({
+      ...Permissions.default(),
+      editState: Permissions.none(),
+      setTokenSymbol: Permissions.none(),
+      //   editActionsState: Permissions.none(),
+      send: Permissions.none(),
+      receive: Permissions.none(),
+      setPermissions: Permissions.none(),
+      incrementNonce: Permissions.proof(),
+    });
+  }
+
+  @method async initialiseUserAccount(
+    address: PublicKey,
+    vk: VerificationKey,
+    // tokenKey: PrivateKey,
+    value: Field
+  ) {
+    // const update = AccountUpdate.createSigned(address, this.tokenId);
+    // update.body.update.verificationKey = { isSome: Bool(true), value: vk };
+    // update.body.update.permissions = {
+    //   isSome: Bool(true),
+    //   value: {
+    //     ...Permissions.default(),
+    //     editState: Permissions.proofOrSignature(),
+    //     incrementNonce: Permissions.proofOrSignature(),
+    //   },
+    // };
+
+    // update.body.update.appState[0] = { isSome: Bool(true), value };
+
+    const deployUpdate = AccountUpdate.createSigned(address, this.tokenId);
+    deployUpdate.account.permissions.set({
+      ...Permissions.default(),
+      receive: Permissions.none(),
+      send: Permissions.proof(),
+      editState: Permissions.proof(),
+      editActionState: Permissions.proof(),
+      setDelegate: Permissions.proof(),
+      setPermissions: Permissions.proof(),
+      setZkappUri: Permissions.proof(),
+      setTokenSymbol: Permissions.proof(),
+      incrementNonce: Permissions.proof(),
+      setVotingFor: Permissions.proof(),
+      setTiming: Permissions.proof(),
+      access: Permissions.none(),
+    });
+    deployUpdate.body.update.verificationKey = {
+      isSome: Bool(true),
+      value: vk,
+    };
+    // deployUpdate.account.verificationKey.set(
+    //   TokenContract.withDrawAccountVerifyKey
+    // );
+    // deployUpdate.account.isNew.assertEquals(Bool(true));
+    AccountUpdate.setValue(deployUpdate.body.update.appState[0], Field(1));
+    AccountUpdate.setValue(deployUpdate.body.update.appState[1], Field(1));
+    deployUpdate.requireSignature();
+  }
+
+  @method async dudd() {
+    let x = this.x.getAndRequireEquals();
+    Provable.log('x: ', x);
+    let y = x.add(Field(1));
+    this.x.set(y);
+    Provable.log('x + 1: ', y);
+  }
+
+  @method async duddToken(user: PrivateKey) {
+    let ud = new UserData(user.toPublicKey(), this.tokenId);
+    await ud.duddToken();
+  }
+
+  @method async initUser(user: PrivateKey, amount: Field) {
+    let ud = new UserData(user.toPublicKey(), this.tokenId);
+    await ud.initialiseUserAccount(user, amount, this.tokenId);
+  }
+
+  @method async updateState(user: PublicKey, seg: UInt64) {
+    let ud = new UserData(user, this.tokenId);
+    const nullStartIndex = await ud.paySegments(seg);
+    Provable.log('nullStartIndex: ', nullStartIndex);
+  }
+}
+
+// Wrapper
+export class UserData extends SmartContract {
+  @state(Field) payments = State<Field>();
+  @state(Field) overpayments = State<Field>();
+  @state(Field) compensations = State<Field>();
+  /** Contract that is allowed to modify state of this token account */
+  @state(PublicKey) group = State<PublicKey>();
+
+  @method async duddToken() {
+    let x = this.payments.getAndRequireEquals();
+    Provable.log('x: ', x);
+  }
+
+  // async init() {
+  //   super.init();
+  // }
+
+  // async deploy(args: DeployArgs & { group: PublicKey }) {
+  //   super.deploy(args);
+  //   this.account.permissions.set({
+  //     ...Permissions.default(),
+  //     editState: Permissions.none(),
+  //     setTokenSymbol: Permissions.none(),
+  //     //   editActionsState: Permissions.none(),
+  //     send: Permissions.none(),
+  //     receive: Permissions.impossible(),
+  //     setPermissions: Permissions.none(),
+  //     incrementNonce: Permissions.proof(),
+  //   });
+
+  //   // Set the admin address
+  //   this.group.set(args.group);
+  // }
+
+  /** Called once at the start. User relinquishes ability to modify token account bu signing */
+  @method async initialiseUserAccount(
+    user: PrivateKey,
+    amount: Field,
+    tokenId: Field
+  ) {
+    // Nullify all other fields
+    // Update struct
+    // let update = AccountUpdate.create(user.toPublicKey(), tokenId);
+    let update = AccountUpdate.createSigned(user.toPublicKey(), tokenId);
+    // Provable.log('start: ', update.body.update.appState[0]);
+    // // let firstState = update.body.update.appState[0];
+    AccountUpdate.setValue(update.body.update.appState[0], amount);
+    // Provable.log('end  : ', update.body.update.appState[0]);
+    // // Sets receive to proof only
+    AccountUpdate.setValue(update.body.update.permissions, {
+      ...Permissions.default(),
+      editState: Permissions.proofOrSignature(),
+      setTokenSymbol: Permissions.proof(),
+      send: Permissions.proof(),
+      receive: Permissions.proof(),
+      setPermissions: Permissions.proof(),
+      incrementNonce: Permissions.none(),
+    });
+
+    update.requireSignature();
+  }
+
+  /** Tick of a single payment round */
+  @method async paySegments(paymentRound: UInt64) {
+    let paymentsField = this.payments.getAndRequireEquals();
+    const payments: Payments = await Payments.fromBools(
+      Payments.unpack(paymentsField)
+    );
+
+    // // Write to the month index provided
+    let paymentsBools: Bool[] = await Payments.unpack(payments.packed);
+
+    // Iterate over all values and flip one only
+    for (let i = 0; i < 240; i++) {
+      let t: Bool = paymentsBools[i];
+      let newWrite: Bool = Provable.if(
+        new UInt64(i).equals(paymentRound),
+        Bool(true),
+        t
+      );
+
+      paymentsBools[i] = newWrite;
+    }
+
+    console.log(
+      'Pay seg paymentRound.value.value[0]: ',
+      paymentRound.value.value[0]
+    );
+
+    // Update payments
+    this.payments.set(Payments.fromBoolsField(paymentsBools));
+  }
+
+  /** Make up for prior missed payments */
+  @method.returns(Field) async totalPayments(): Promise<Field> {
+    // Extract compensations
+    let compensationsField = this.compensations.getAndRequireEquals();
+    const compensations: Payments = Payments.fromBools(
+      Payments.unpack(compensationsField)
+    );
+
+    let compensationBools: Bool[] = Payments.unpack(compensations.packed);
+
+    // Extract payments
+    let paymentsField = this.payments.getAndRequireEquals();
+    const payments: Payments = Payments.fromBools(
+      Payments.unpack(paymentsField)
+    );
+
+    let paymentsBools: Bool[] = Payments.unpack(payments.packed);
+
+    // Variable for total payments count
+    let count: Field = Field(0);
+
+    // Loop over both
+    for (let i = 0; i < 240; i++) {
+      let add_payments: Field = Provable.if(
+        paymentsBools[i].equals(true),
+        Field(1),
+        Field(0)
+      );
+
+      let add_compensation: Field = Provable.if(
+        compensationBools[i].equals(true),
+        Field(1),
+        Field(0)
+      );
+
+      // Add to count
+      count = count.add(add_payments).add(add_compensation);
+    }
+
+    // Add any overpayments
+    return count;
+  }
+
+  /** Gate for lottery */
+  @method.returns(Bool) async lotteryAccess(
+    currentSegment: Field
+  ): Promise<Bool> {
+    // Get payments so far
+    const totalPayments: Field = await this.totalPayments();
+
+    // Return true if it equals current segment number
+    return totalPayments.equals(currentSegment);
+  }
+
+  /** Make up for prior missed payments */
+  @method async compensate(numberOfCompensations: UInt64) {
+    // Extract compensations
+    let compensationsField = this.compensations.getAndRequireEquals();
+    const compensations: Payments = await Payments.fromBools(
+      Payments.unpack(compensationsField)
+    );
+    let compensationBools: Bool[] = await Payments.unpack(compensations.packed);
+
+    // Extract payments
+    let paymentsField = this.payments.getAndRequireEquals();
+    const payments: Payments = await Payments.fromBools(
+      Payments.unpack(paymentsField)
+    );
+
+    let paymentsBools: Bool[] = await Payments.unpack(payments.packed);
+
+    // console.log('paymentsField', paymentsField);
+
+    let change: Bool;
+
+    // Iterate over untill the end
+    for (let i = 0; i < 240; i++) {
+      // console.log('Loop vallue: ', paymentsBools[i]);
+      // Change will occur if there is enough to pay and this month is to be paid
+      change = Provable.if(
+        // numberOfCompensations
+        //   .greaterThan(new UInt64(0)) // Something left to pay off
+        //   .and(paymentsBools[i].equals(Bool(false))), // This entry has not been paid
+        paymentsBools[i].equals(Bool(false)),
+        Bool(true),
+        Bool(false)
+      );
+
+      // Update array of compensations
+      compensationBools[i] = await Provable.if(change, Bool(true), Bool(false));
+
+      // Set the amount to be subtracted
+      let subAmount: UInt64 = await Provable.if(
+        change,
+        new UInt64(1),
+        new UInt64(0)
+      );
+
+      // Deduct from numberOfCompensations
+      numberOfCompensations = numberOfCompensations.sub(subAmount);
+    }
+
+    // Update compensations
+    this.compensations.set(Payments.fromBoolsField(compensationBools));
+  }
+}
