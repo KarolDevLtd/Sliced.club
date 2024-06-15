6816e20e2db4b76a77a10126459d68169513006b
Scott Phillips <36128399+ScottJamesPhillips@users.noreply.github.com>
2024-06-15 10:00:58 +0100

    refactor: :recycle: basic bar chart dynamically populating

--- client/src/app/_components/ui/BasicBarChart.tsx
+++ client/src/app/_components/ui/BasicBarChart.tsx
@@ -1,38 +1,51 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import React, { useEffect, useState } from 'react';
 import { BarChart, XAxis, YAxis, Bar, Tooltip, ResponsiveContainer, CartesianGrid } from 'recharts';
+import { PaymentBarChartData } from '~/static-data';
 
-const BasicBarChart = () => {
-	const data = [
-		{
-			name: 'Jan',
-			moneys: 45000,
-		},
-		{
-			name: 'Feb',
-			moneys: 40000,
-		},
-		{
-			name: 'Mar',
-			moneys: 30000,
-		},
-		{
-			name: 'Apr',
-			moneys: 12500,
-		},
-		{
-			name: 'May',
-			moneys: 7500,
-		},
-	];
+type BasicBarChartProps = {
+	chartData: [];
+};
+
+const BasicBarChart = ({ chartData }: BasicBarChartProps) => {
+	const [highestPrice, setHighestPrice] = useState(0);
+	const [lowestPrice, setLowestPrice] = useState(0);
 
 	// Formatter function
 	const formatCurrency = (value) => {
 		return `$${(value / 1000).toFixed(0)}K`;
 	};
 
+	//Not used atm but nice to have incase
+	const getHighLowValues = () => {
+		let highestPricedItem;
+		let lowestPricedItem;
+
+		if (chartData.length > 0) {
+			highestPricedItem = chartData[0];
+			lowestPricedItem = chartData[0];
+
+			for (const item of chartData) {
+				if (parseFloat(item.moneys) > parseFloat(highestPricedItem.moneys)) {
+					highestPricedItem = item;
+				}
+				if (parseFloat(item.moneys) < parseFloat(lowestPricedItem.moneys)) {
+					lowestPricedItem = item;
+				}
+			}
+		}
+
+		setHighestPrice(highestPricedItem.moneys);
+		setLowestPrice(lowestPricedItem.moneys);
+	};
+
+	useEffect(() => {
+		getHighLowValues();
+	}, [chartData]);
+
 	return (
 		<ResponsiveContainer width="100%" height={250}>
-			<BarChart data={data} barSize={20}>
+			<BarChart data={chartData} barSize={20}>
 				<defs>
 					<linearGradient id="colorHigh" x1="0" y1="0" x2="0" y2="1">
 						<stop offset="0%" stopColor="#71c36c" stopOpacity={1} />
@@ -44,8 +57,8 @@ const BasicBarChart = () => {
 					fontSize={12}
 					axisLine={false}
 					tickFormatter={formatCurrency}
-					domain={[0, 50000]}
-					ticks={[0, 10000, 20000, 30000, 40000, 50000]}
+					tickCount={5}
+					domain={['auto', 'auto']}
 				/>
 				<CartesianGrid vertical={false} stroke="#666666" />
 				<Tooltip formatter={(value) => formatCurrency(value)} />
